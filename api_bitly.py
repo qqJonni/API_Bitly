5ca2a510dc2a2a33fdd499406ab3a9a9950faed6
Valeriy Lvov <tepliy_shov@mail.ru>
2023-09-27 00:27:32 +0500

    step 4

--- api_bitly.py
+++ api_bitly.py
@@ -3,21 +3,23 @@ from dotenv import load_dotenv, find_dotenv
 import requests
 
 
-def start():
+def shorten_link(token, url):
     load_dotenv(find_dotenv())
-    token = os.environ.get('BITLY_TOKEN')
-    long_url = 'http://dvmn.org'
 
     headers = {
         'Authorization': f'Bearer {token}',
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
     }
 
-    data = f'{{ "long_url": "{long_url}" }}'
+    data = f'{{ "long_url": "{url}" }}'
     response = requests.post('https://api-ssl.bitly.com/v4/bitlinks', headers=headers, data=data)
 
-    print(response.json())
+    if response.status_code == 200:
+        bitlink = response.json().get('link')
+        print(f'Битлинк: {bitlink}')
+    else:
+        print('Error occurred.')
 
 
 if __name__ == '__main__':
-    start()
+    shorten_link(input('Enter token: '), input('Enter url: '))
