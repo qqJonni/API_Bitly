7974f033ff1898d7159d6f9478ad8ed638ce763a
Valeriy Lvov <tepliy_shov@mail.ru>
2023-09-26 22:46:18 +0500

    step 2

--- api_bitly.py
+++ api_bitly.py
@@ -1,10 +1,14 @@
 import os
 from dotenv import load_dotenv, find_dotenv
+import requests
 
 
 def start():
     load_dotenv(find_dotenv())
     token = os.environ.get('BITLY_TOKEN')
+    headers = {'Authorization': token}
+    response = requests.get('https://api-ssl.bitly.com/v4/user', headers=headers)
+    print(response.json())
 
 
 if __name__ == '__main__':
